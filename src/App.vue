5c47f7e85853c3d35fa4f0a6861b6e3d3225d347
Rafli Haidar <rafli.haidar30@gmail.com>
2024-04-07 22:26:29 +0700

    feat(App) : add global navbar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
-<script setup lang="ts">
-import HomeView from './views/HomeView.vue';
-</script>
-
 <template>
+  <BaseNavbar />
   <router-view />
 </template>
+
+<script setup lang="ts">
+import BaseNavbar from './components/BaseNavbar.vue';
+</script>
